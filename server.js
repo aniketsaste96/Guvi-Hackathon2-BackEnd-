25946f75e6962e56cd630c98542fe7309b6b3103
Aniket <aniketsaste96@gmail.com>
2022-01-20 00:46:14 +0530

    backend

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 const app = express()
 app.use(express.json());
 app.use(express.urlencoded())
